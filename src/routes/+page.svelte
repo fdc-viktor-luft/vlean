8709e07ceaa5222578c0bfdcb6a7fde315af9665
Viktor Luft <viktor.luft@freiheit.com>
2024-04-24 22:06:23 +0200

    Add file access

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 
+	let workdir: FileSystemDirectoryHandle | undefined = undefined;
+	const onClick = async () => {
+		workdir = await showDirectoryPicker().catch(() => undefined);
+		if (!workdir) return;
+		for await (const [key, value] of workdir.entries()) {
+  			console.log({ key, value: value.kind === 'file' ? await value.getFile() : 'is dir' });
+		}
+	}
 </script>
 
 <div>
-	WIP
+	<button on:click={onClick}>Click Me</button>
 </div>
 
 <style lang="scss">
